b4fd29591aab850b901a3db4ad1cd93759b0fb74
JoshRoberts <j-et@live.co.uk>
2021-02-10 17:44:14 +0000

    Changed the favicon to icon, changed the footer for correct formatting.

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -2,12 +2,12 @@ import React from 'react';
 
 export default function Footer() {
     return (
-        <div className='container my-5 static bottom-0'>
+        <footer className='footer'>
             <div className='flex justify-center pb-4 divide-x divide-gray-400'>
                 <a href='https://www.linkedin.com/in/joshuaetroberts' className='text-xl px-5 hover:text-yellow-700'>LinkedIn</a>
                 <a href='https://www.facebook.com/JoshuaEdwinTudorRoberts' className='text-xl px-5 hover:text-yellow-700'>Facebook</a>
                 <a href='https://www.instagram.com/jet1998/' className='text-xl px-5 hover:text-yellow-700'>Instagram</a>
             </div>
-        </div>
+        </footer>
     );
 }
